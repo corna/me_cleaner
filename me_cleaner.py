b11264123ae8d491ac5699163a2429c5fc9f8580
Nicola Corna <nicola@corna.info>
2016-12-15 15:51:01 +0100

    Detect full images and adjust the offsets accordingly

--- me_cleaner.py
+++ me_cleaner.py
@@ -53,130 +53,155 @@ def remove_module(f, mod_header, ftpr_offset, lzma_start_rel, lzma_end_rel):
         print(" {:<16}: unknown compression, skipping".format(name))
 
 if len(sys.argv) != 2 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print("Usage: me_cleaner.py me_image.bin")
+    print("Usage: \n"
+          " me_cleaner.py me_image.bin\n"
+          "or\n"
+          " me_cleaner.py full_dump.bin")
 else:
     with open(sys.argv[1], "r+b") as f:
         f.seek(0x10, 0)
-        fpt_header = f.read(4)
+        magic = f.read(4)
 
-        if fpt_header == b"$FPT":
-            print("Found FPT header at 0x10")
+        me_start = 0
+        me_end = 0
 
-            entries = unpack("<I", f.read(4))[0]
-            print("Found {} partition(s)".format(entries))
+        if magic == b"$FPT":
+            print("ME image detected")
+            me_start = 0
+            f.seek(0, 2)
+            me_end = f.tell()
 
+        elif magic == b"\x5a\xa5\xf0\x0f":
+            print("Full image detected")
             f.seek(0x14, 0)
-            header_len = unpack("<B", f.read(1))[0]
-            f.seek(0x28, 0)
-            version = unpack("<HHHH", f.read(8))
+            flmap0 = unpack("<I", f.read(4))[0]
+            nr = flmap0 >> 24 & 0x7
+            frba = flmap0 >> 12 & 0xff0
+            if nr >= 2:
+                f.seek(frba + 0x8, 0)
+                flreg2 = unpack("<I", f.read(4))[0]
+                me_start = (flreg2 & 0x1fff) << 12
+                me_end = flreg2 >> 4 & 0x1fff000 | 0xfff
 
-            if version == (0, 0, 0, 0):
-                print("Unspecified ME firmware version")
+                if me_start >= me_end:
+                    sys.exit("The ME region in this image has been disabled")
+                
+                f.seek(me_start + 0x10)
+                if f.read(4) != b"$FPT":
+                    sys.exit("The ME region is corrupted or missing")
             else:
-                print("ME firmware version {}"
-                      .format('.'.join(str(i) for i in version)))
-
-            f.seek(0x30, 0)
-            partitions = f.read(entries * 0x20)
-
-            ftpr_header = b""
-            ftpr_offset = 0
-            ftpr_lenght = 0
-
-            for i in range(entries):
-                if partitions[i * 0x20:(i * 0x20) + 4] == b"FTPR":
-                    ftpr_header = partitions[i * 0x20:(i + 1) * 0x20]
-                    break
-
-            if ftpr_header != b"":
-                ftpr_offset, ftpr_lenght = unpack("<II", ftpr_header[0x08:0x10])
-                print("Found FTPR header: FTPR partition spans from "
-                      "0x{:02x} to 0x{:02x}".format(ftpr_offset,
-                                                    ftpr_offset + ftpr_lenght))
-                print("Removing extra partitions...")
-
-                fill_range(f, 0x30, ftpr_offset, b"\xff")
-                f.seek(0, 2)
-                fill_range(f, ftpr_offset + ftpr_lenght, f.tell(), b"\xff")
-
-                print("Removing extra partition entries in FPT...")
-                f.seek(0x30, 0)
-                f.write(ftpr_header)
-                f.seek(0x14, 0)
-                f.write(pack("<I", 1))
-
-                print("Removing EFFS presence flag...")
-                f.seek(0x24, 0)
-                flags = unpack("<I", f.read(4))[0]
-                flags &= ~(0x00000001)
-                f.seek(0x24, 0)
-                f.write(pack("<I", flags))
-
-                f.seek(ftpr_offset, 0)
-                if version[0] < 11 and f.read(4) != b"$CPD":
-                    print("Reading FTPR modules list...")
-                    f.seek(ftpr_offset + 0x1c, 0)
-                    tag = f.read(4)
-                    if tag == b"$MN2":
-                        f.seek(ftpr_offset + 0x20, 0)
-                        num_modules = unpack("<I", f.read(4))[0]
-                        f.seek(ftpr_offset + 0x290, 0)
-                        mod_hs = [f.read(0x60) for i in range(0, num_modules)]
-
-                        if any(mod_h.startswith(b"$MME") for mod_h in mod_hs):
-                            f.seek(ftpr_offset + 0x18, 0)
-                            size = unpack("<I", f.read(4))[0]
-                            llut_start = ftpr_offset + (size * 4 + 0x3f) & ~0x3f
-
-                            f.seek(llut_start + 0x10, 0)
-                            huff_start, huff_size = unpack("<II", f.read(8))
-                            lzma_start = huff_start + huff_size
-
-                            print("Wiping LZMA section (0x{:0X} - 0x{:0X})"
-                                  .format(lzma_start,
-                                          ftpr_offset + ftpr_lenght))
-                            fill_range(f, lzma_start,
-                                       ftpr_offset + ftpr_lenght, b"\xff")
-
-                            f.seek(llut_start, 0)
-                            if f.read(4) == b"LLUT":
-                                for mod_header in mod_hs:
-                                    remove_module(f, mod_header, ftpr_offset,
-                                                  lzma_start - ftpr_offset,
-                                                  ftpr_lenght)
-
-                            else:
-                                print("Can't find the LLUT region in the FTPR "
-                                      "partition")
-                        else:
-                            print("Can't find the $MN2 modules in the FTPR "
-                                  "partition")
-                    else:
-                        print("Wrong FTPR partition tag ({})".format(tag))
-
-                else:
-                    print("Modules removal in ME v11 or greater is not yet "
-                          "supported")
-
-                print("Correcting checksum...")
-                # The checksum is just the two's complement of the sum of the
-                # first 0x30 bytes (except for 0x1b, the checksum itself). In
-                # other words, the sum of the first 0x30 bytes must be always
-                # 0x00.
-                f.seek(0x00, 0)
-                checksum_bytes = f.read(0x30)
-                f.seek(0x1b, 0)
-                f.write(pack("B", (0x100 -
-                             (sum(checksum_bytes) - checksum_bytes[0x1b]) &
-                             0xff) & 0xff))
-
-                print("Done! Good luck!")
-
-            else:
-                print("FTPR header not found, this image doesn't seem to be "
-                      "valid")
-
+                sys.exit("This image does not contains an ME firmware (NR = {})"
+                         .format(nr))
         else:
-            print("{} is not a valid Intel ME image (FPT not found)"
-                  .format(sys.argv[1]))
+            sys.exit("Unknown image")
+
+        print("Found FPT header at 0x{:0X}".format(me_start + 0x10))
+
+        f.seek(me_start + 0x14)
+        entries = unpack("<I", f.read(4))[0]
+        print("Found {} partition(s)".format(entries))
+
+        f.seek(me_start + 0x14, 0)
+        header_len = unpack("<B", f.read(1))[0]
+        f.seek(me_start + 0x28, 0)
+        version = unpack("<HHHH", f.read(8))
+
+        if version == (0, 0, 0, 0):
+            print("Unspecified ME firmware version")
+        else:
+            print("ME firmware version {}"
+                  .format('.'.join(str(i) for i in version)))
+
+        f.seek(me_start + 0x30, 0)
+        partitions = f.read(entries * 0x20)
+
+        ftpr_header = b""
+        ftpr_offset = 0
+        ftpr_lenght = 0
+
+        for i in range(entries):
+            if partitions[i * 0x20:(i * 0x20) + 4] == b"FTPR":
+                ftpr_header = partitions[i * 0x20:(i + 1) * 0x20]
+                break
+
+        if ftpr_header == b"":
+            sys.exit("FTPR header not found, this image doesn't seem to be "
+                     "valid")
+
+        ftpr_offset, ftpr_lenght = unpack("<II", ftpr_header[0x08:0x10])
+        ftpr_offset += me_start
+        print("Found FTPR header: FTPR partition spans from "
+              "0x{:02x} to 0x{:02x}".format(ftpr_offset,
+                                            ftpr_offset + ftpr_lenght))
+        print("Removing extra partitions...")
+
+        fill_range(f, me_start + 0x30, ftpr_offset, b"\xff")
+        fill_range(f, ftpr_offset + ftpr_lenght, me_end, b"\xff")
+
+        print("Removing extra partition entries in FPT...")
+        f.seek(me_start + 0x30, 0)
+        f.write(ftpr_header)
+        f.seek(me_start + 0x14, 0)
+        f.write(pack("<I", 1))
+
+        print("Removing EFFS presence flag...")
+        f.seek(me_start + 0x24, 0)
+        flags = unpack("<I", f.read(4))[0]
+        flags &= ~(0x00000001)
+        f.seek(me_start + 0x24, 0)
+        f.write(pack("<I", flags))
+
+        f.seek(ftpr_offset, 0)
+        if version[0] < 11 and f.read(4) != b"$CPD":
+
+            print("Reading FTPR modules list...")
+            f.seek(ftpr_offset + 0x1c, 0)
+            tag = f.read(4)
+
+            if tag != b"$MN2":
+                print("Wrong FTPR partition tag ({})".format(tag))
+
+            f.seek(ftpr_offset + 0x20, 0)
+            num_modules = unpack("<I", f.read(4))[0]
+            f.seek(ftpr_offset + 0x290, 0)
+            mod_hs = [f.read(0x60) for i in range(0, num_modules)]
+
+            if not any(mod_h.startswith(b"$MME") for mod_h in mod_hs):
+                sys.exit("Can't find the $MN2 modules in the FTPR "
+                         "partition")
+
+            f.seek(ftpr_offset + 0x18, 0)
+            size = unpack("<I", f.read(4))[0]
+            llut_start = ftpr_offset + (size * 4 + 0x3f) & ~0x3f
+
+            f.seek(llut_start + 0x10, 0)
+            huff_start, huff_size = unpack("<II", f.read(8))
+            huff_start += me_start
+            lzma_start = huff_start + huff_size
+
+            print("Wiping LZMA section (0x{:0X} - 0x{:0X})"
+                  .format(lzma_start, ftpr_offset + ftpr_lenght))
+            fill_range(f, lzma_start, ftpr_offset + ftpr_lenght, b"\xff")
+
+            f.seek(llut_start, 0)
+            if f.read(4) != b"LLUT":
+                sys.exit("Can't find the LLUT region in the FTPR partition")
+
+            for mod_header in mod_hs:
+                remove_module(f, mod_header, ftpr_offset,
+                              lzma_start - ftpr_offset, ftpr_lenght)
+        else:
+            print("Modules removal in ME v11 or greater is not yet supported")
+
+        print("Correcting checksum...")
+        # The checksum is just the two's complement of the sum of the first
+        # 0x30 bytes (except for 0x1b, the checksum itself). In other words,
+        # the sum of the first 0x30 bytes must be always 0x00.
+        f.seek(me_start, 0)
+        checksum_bytes = f.read(0x30)
+        f.seek(me_start + 0x1b, 0)
+        f.write(pack("B", (0x100 - (sum(checksum_bytes) - checksum_bytes[0x1b])
+                           & 0xff) & 0xff))
+
+        print("Done! Good luck!")
 
